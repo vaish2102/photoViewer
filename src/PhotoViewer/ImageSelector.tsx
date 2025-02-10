7f9c809aec6ed30441ba01c8ccb54ac0a4d6441f
Suet Kee <suet.kee@softwire.com>
2025-02-10 15:54:15 +0000

    incomplete modification to image selector

--- src/PhotoViewer/ImageSelector.tsx
+++ src/PhotoViewer/ImageSelector.tsx
@@ -1,9 +1,36 @@
-import React from "react";  // import React (to provide access to TSX)
+import React from "react"; 
+import { useState } from 'react'
+import './ImageSelector.css';
+ 
 
-export function ImageSelector() {    // declare and export new function called 'ImageSelector'
-    return (                
-        <div>              
-            Hello World!
+const brokenImages = [ 1, 24, 32, 36, 44, 47];
+function getImageUrls() {
+    const urls = [];
+    for (let i = 0; i < 50; i++) {
+        if (!brokenImages.includes(i)) {
+            const imageNumberString = i.toString().padStart(2, '0');
+            urls.push(`https://picsum.photos/id/6${imageNumberString}/1600/900.jpg`)
+        }
+    }
+    return urls;
+}
+
+export const imageUrls = getImageUrls();  
+
+export function ImageSelector(onChange:any) {  
+    return ( 
+        <div className="thumbnailDiv">
+            {
+            imageUrls.map((url) => <div> <img className="thumbnailImage" src={url} onClick={onChange(url)}/> </div>)
+            }
         </div>
+        
     );
 }
+
+
+
+
+
+
+
--- src/PhotoViewer/PhotoViewer.tsx
+++ src/PhotoViewer/PhotoViewer.tsx
@@ -2,8 +2,8 @@ import React from "react";  // import React (to provide access to TSX)
 import './PhotoViewer.css';
 export  function PhotoViewer(props:{src: string | undefined}) {    // declare and export new function called 'PhotoViewer'
     return (                
-        <div className="mainphoto">               
-            <img src ={props.src} />
+        <div className="mainImgDiv">               
+            <img className="mainImg" src ={props.src} />
             
         </div>
     );
